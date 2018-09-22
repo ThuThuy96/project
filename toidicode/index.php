0c98870890555e3ae6f1f3e8beddee4885ad064e
ThuThuy96 <thuthuytut@gmail.com>
2018-09-23 04:02:50 +0700

    baitap

---
+++ toidicode/index.php
@@ -0,0 +1,6 @@
+<?php require "header.php"; ?>
+ <div class="content_row" >
+     <?php require "main_content.php"; ?>
+     <?php require "sidebar.php";?>
+ </div>
+<?php require "footer.php"; ?>
---
+++ toidicode/sidebar.php
@@ -0,0 +1,63 @@
+<aside id="sidebar" class="sidebar right">
+    <div id="" class="" style="margin-bottom: 30px; overflow-x: : hidden;">
+        <h4 class="widget-title">QC</h4>
+
+        <!--     bai-viet
+            head-widget -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:300px;height:250px"
+             data-ad-client="ca-pub-8768260344279113"
+             data-ad-slot="4493528180"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>            <div id="" class="widget widget_text elp-widget">
+        <h4 class="widget-title">Linh Tinh</h4>
+        <ul>
+            <li><a href="https://toidicode.com/tich-hop-va-su-dung-codeigniter-customs-301.html" title="T&iacute;ch hợp v&agrave; sử dụng CodeIgniter Customs" >T&iacute;ch hợp v&agrave; sử dụng CodeIgniter Customs</a></li>
+            <li><a href="https://toidicode.com/tong-hop-cac-trang-thai-http-va-noi-dung-cua-no-327.html" title="Tổng hợp c&aacute;c trạng th&aacute;i HTTP v&agrave; nội dung của n&oacute;" >Tổng hợp c&aacute;c trạng th&aacute;i HTTP v&agrave; nội dung của n&oacute;</a></li>
+            <li><a href="https://toidicode.com/huong-dan-tao-bo-chuyen-doi-tieng-viet-kieu-moi-375.html" title="Hướng dẫn tạo bộ chuyển đổi Tiếng Việt kiểu mới" >Hướng dẫn tạo bộ chuyển đổi Tiếng Việt kiểu mới</a></li>
+        </ul>
+    </div>            <div id="email-subscribers-2" class="widget widget_text elp-widget"><h5 class="widget-title">ĐĂNG KÝ NHẬN BÀI QUA EMAIL</h5>
+        <script src="https://toidicode.com/public/ajax/ajaxmail.min.js?v=1.1"></script>
+        <div>
+            <div class="es_msg"><font color='red' id="es_msg"></font></div>
+            <div class="es_msgc"><font color='green' id="es_msgc"></font></div>
+            <div class="es_lablebox">Email *</div>
+            <div class="es_textbox">
+                <form action="javascript:void(0)" onsubmit="subMail('8085bbcd02c17376941e6af0e5eb5371')" autocomplete="off">
+                    <input class="es_textbox_class" type="email" name="email_feild" id="email_feild" onkeypress="if(event.keyCode==13) submail()" placeholder="Mail..." minlength="6" maxlength="30" required="">
+            </div>
+            <div class="es_button" style="margin-top: 5px;">
+                <input class="es_textbox_button" name="es_txt_button" id="es_txt_button" value="Subscribe" type="submit">
+                </form>
+            </div>
+        </div>
+    </div>            <div class="widget widget_text elp-widget">
+        <h4 class="widget-title">Tools</h4>
+        <ul class="listtools">
+            <li><a href="https://chrome.google.com/webstore/detail/toidicodecom-extension/geinliihnjpahdmepmimcjjendjbgijm?hl=vi" target="_bank" title="Toidicode extension">Toidicode Extension</a></li>
+            <li><a href="https://toidicode.com/face-transform.html" target="_bank" title="Face transform">Face Transform</a></li>
+            <li><a href="https://youtubevi.com" target="_bank" title="Download Video From Youtube">Download Video From Youtube</a></li>
+            <li><a href="https://chrome.google.com/webstore/detail/youtube-downloader/gkaihnefbbjhdhnpkiphldicihhpchnm" target="_bank" title="Face transform">Youtube Downloader</a></li>
+        </ul>
+    </div>
+    <div id="" class="">
+        <h4 class="widget-title">FANPAGE</h4>
+        <div class="fb-page" data-href="https://www.facebook.com/toidicode/" data-tabs="false" data-width="300" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true">
+            <blockquote cite="https://www.facebook.com/toidicode/" class="fb-xfbml-parse-ignore">
+                <a href="https://www.facebook.com/toidicode/">Toidicode.com</a>
+            </blockquote>
+        </div>
+    </div>            <!-- ads -->
+    <div id="" class="widget widget_text elp-widget vce-sticky">
+        <!-- fix-top -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:300px;height:600px"
+             data-ad-client="ca-pub-8768260344279113"
+             data-ad-slot="9202929389"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>        </aside>
+</div>
